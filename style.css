a6bd3eb356b6bbab7cfc8ef4ab5f787e068abc10
gabriel1p2c <140512025+gabriel1p2c@users.noreply.github.com>
2023-07-25 09:58:56 -0300

    uploud

---
+++ style.css
@@ -0,0 +1,27 @@
+let xBolinha = 300;
+let yBolinha = 200;
+let diametro = 25;
+let velocidadexbolinha = 50;
+let velocidadeybolinha = 50;
+
+function setup() {
+  createCanvas(600, 400);
+}
+
+function draw() {
+  background(0);
+
+  circle(xBolinha, yBolinha, diametro);
+  xBolinha += velocidadexbolinha;
+  yBolinha += velocidadeybolinha;
+  
+  if(xBolinha >width || xBolinha < 0){
+    velocidadexbolinha *= -1;
+  }
+  if(yBolinha >width || yBolinha < 0){
+    velocidadeybolinha *= -1;
+  }
+  
+
+ 
+}
